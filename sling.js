5b6087e660ca98ba3e0ed81c1d5a431803bdc8b9
MohitRaheja4 <75528150+MohitRaheja4@users.noreply.github.com>
2021-03-04 16:20:53 +0530

    Update sling.js

--- sling.js
+++ sling.js
@@ -11,7 +11,7 @@ class sling{
         this.chain=Constraint.create(op)
         World.add(world,this.chain);
     }
-     attach(bodyA){
+     attach(body,bodyA){
         this.sling.bodyA = body;
     }
     
